7f8b796d273f424653423c9eab1aec5191716fac
YR <myccpb08@gmail.com>
2019-07-14 17:29:37 +0900

    일요일

--- src/components/gitgraph.vue
+++ src/components/gitgraph.vue
@@ -1,71 +1,66 @@
 <template>
-<div style="margin-top: 200px;">
-  <div class="calendarContainer">
-    <div class="calendarBox" id="calendar_basic" style="width: 1000px; height: 350px;"></div>
+  <div style="margin-top: 50px;">
+    <div class="calendarContainer">
+      <div class="calendarBox" id="calendar_basic" style="width: 800px; height: 250px;"></div>
+    </div>
+    <v-btn fab outline large color="cyan" v-on:click="getCommits()">프로젝트</v-btn>
+    <v-btn fab outline color="success" v-on:click="getRepos('myccpb08','5yRamVkqs4Z4bq-G1roY')">유림</v-btn>
+    <v-btn fab outline color="success" v-on:click="getRepos('Kim_yh', 'N9RKhWdxvbGzn3oYEwVe')">영훈</v-btn>
+    <v-btn fab outline color="success" v-on:click="getRepos('JIGyeongmin', 'yYcb5LEDsxxbN1PPxKEj')">경민</v-btn>
+    <v-btn fab outline color="success" v-on:click="getRepos('LeeSuKyeong','dCp7MpuwFQNzYrLBZix5')">수경</v-btn>
+    <v-btn fab outline color="success" v-on:click="getRepos('seok','xTftb51x12NTwFbxxAC5')">주연</v-btn>
   </div>
-  <v-btn v-on:click="getCommits()">프로젝트</v-btn>
-  <v-btn v-on:click="getRepos('myccpb08','5yRamVkqs4Z4bq-G1roY')">나</v-btn>
-  <v-btn v-on:click="getRepos('Kim_yh', 'N9RKhWdxvbGzn3oYEwVe')">영훈</v-btn>
-  <v-btn v-on:click="getRepos('JIGyeongmin', 'yYcb5LEDsxxbN1PPxKEj')">경민</v-btn>
-  <v-btn v-on:click="getRepos('LeeSuKyeong','dCp7MpuwFQNzYrLBZix5')">수경</v-btn>
-  <v-btn v-on:click="getRepos('seok','xTftb51x12NTwFbxxAC5')">주연</v-btn>
-</div>
 </template>
 
 <script>
-
 const BASE_URL = "https://lab.ssafy.com/api/v4";
 google.charts.load("current", {
   packages: ["calendar"]
 });
 google.charts.setOnLoadCallback(drawChart);
 
-$(function() {
-  drawChart();
-});
-
 // https://developers.google.com/chart/interactive/docs/gallery/calendar
 function drawChart(datas) {
-  let personal_title = "그래프"
+  let personal_title =  datas[0].author.name + " " + datas[0].author.username ;
   var dataTable = new google.visualization.DataTable();
   var timeline = [];
   var cnt = [];
   var info = [];
 
-  if (datas != undefined) { // datas 로 반복문 돌림
+  if (datas != undefined) {
+    // datas 로 반복문 돌림
     for (let index = 0; datas[index] != null; index++) {
       var data = datas[index].created_at;
-      if(data == null) continue;
+      if (data == null) continue;
 
-      var year = new Date(data).getFullYear()
-      var month = new Date(data).getMonth() + 1
-      var day = new Date(data).getDate()
-      var temp = year + ',' + month + ',' + day
+      var year = new Date(data).getFullYear();
+      var month = new Date(data).getMonth() + 1;
+      var day = new Date(data).getDate();
+      var temp = year + "," + month + "," + day;
 
-      var cccnt = 1
-      var len = timeline.length
+      var cccnt = 1;
+      var len = timeline.length;
       for (var idx = 0; idx <= len; idx++) {
         if (idx == len) {
-          timeline.push(temp)
-          cnt.push(cccnt)
-          break
+          timeline.push(temp);
+          cnt.push(cccnt);
+          break;
         } else if (timeline[idx] == temp) {
-          cnt[idx]++
-          break
+          cnt[idx]++;
+          break;
         }
       }
-    }  // for문 끝
+    } // for문 끝
 
-    var a = timeline.length
-    for (let idx = 0; idx<a; idx++){
-      var temp = [new Date(timeline[idx]), cnt[idx]]
-      info.push(temp)
+    var a = timeline.length;
+    for (let idx = 0; idx < a; idx++) {
+      var temp = [new Date(timeline[idx]), cnt[idx]];
+      info.push(temp);
     }
 
     dataTable.addColumn({ type: "date", id: "Date" });
     dataTable.addColumn({ type: "number", id: "Won/Loss" });
     dataTable.addRows(info);
-    // console.log(timeline)
 
     var chart = new google.visualization.Calendar(
       document.getElementById("calendar_basic")
@@ -73,117 +68,138 @@ function drawChart(datas) {
 
     var options = {
       title: personal_title,
-      height: 500,
+      height: 300,
       colorAxis: {
-          maxValue: 20,
-          minValue: 0,
-          colors:['white','orange'],
+        maxValue: 30,
+        minValue: 0,
+        colors: ["#faed7d", "ff5e00"]
       },
-      calendar:{
+      calendar: {
+        underMonthSpace: 10,
+
+        monthLabel: {
+        fontName : 'BBTreeGR',
+        fontSize: 12,
+        color: '#8C8C8C',
+      },
         monthOutlineColor: {
-          stroke: '#D8AF91',
+          stroke: "#D8AF91",
           strokeOpacity: 0.5,
           strokeWidth: 1
         },
-      },
+        cellSize: 13,
 
+        dayOfWeekLabel:{
+          fontName : 'BBTreeGR',
+          fontSize:8,
+          color: 'black',
+          bold: true,
+          italic: true,
+          color:  '#8C8C8C',
+        },
+        dayOfWeekRightSpace: 10,
+      },
+      noDataPattern : {
+        backgroundColor : '#EAEAEA', // 줄무늬
+        color : '#F6F6F6'
+      },
     };
 
     chart.draw(dataTable, options);
   }
 } // draw chart 끝
 
-////// 우리프로젝트에 총 commit 수가 몇 개 인지 알아오기
-// var request = require('request');
-// var headers = {'PRIVATE-TOKEN': '5yRamVkqs4Z4bq-G1roY'};
-// var options = {
-//     url: 'https://lab.ssafy.com/api/v4/projects/6097/repository/commits?',
-//     headers: headers
-// };
-//
-// function callback(error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       console.log(response.headers['x-total'])
-//
-//     }
-// }
-//
-// request(options, callback);
-
-
-
-
-
 /////////////////////////////////////
 
 export default {
   data() {
     return {
-      ret: "",
-      ret2: ""
     };
   },
   methods: {
     getRepos(id, token) {
-      fetch(
-          `${BASE_URL}/users/${id}/events?namespaces&per_page=100&private_token=${token}`
-        )
-
-        .then(res => {
-          return res.json();
-        })
-
-
-        .then(data => {
-          //this.ret2 = data;  // data = contribution 들의 집합
-          drawChart(data);
-        });
-    },
-
-    // 프로젝트 commit 가져오는 함수
-    getCommits(id) {
-      fetch(
-        `${BASE_URL}/projects/6097/repository/commits??namespaces&per_page=100&private_token=5yRamVkqs4Z4bq-G1roY`
-      )
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          this.ret = data;
-        });
-
-      // 몇 번 요청 보내야 하는지
-      var request = require('request');
-      var headers = {'PRIVATE-TOKEN': '5yRamVkqs4Z4bq-G1roY'};
-      var options = {
-          url: 'https://lab.ssafy.com/api/v4/projects/6097/repository/commits?',
-          headers: headers
+      var request = require("request");
+      var headers = {
+        "PRIVATE-TOKEN": token
       };
-      f
+      var options = {
+        url: "https://lab.ssafy.com/api/v4/users/" + id + "/events",
+        headers: headers
+      };
+      
       function callback(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var howmany = response.headers['x-total']
-            return howmany
-            // console.log(howmany)
-          }
-      }
+        if (!error && response.statusCode == 200) {
+          var howmany = response.headers["x-total"];
+          var total_try = parseInt(howmany / 100) + 1;
+          var datass = []
+
+          // total_try 만큼 요청보내기
+          for (var trying = 0; trying < total_try; trying++) {
+            fetch(
+              `${BASE_URL}/users/${id}/events?namespaces&per_page=100&page=${trying+1}&private_token=${token}`
+            )
+              .then(res => {
+                return res.json();
+              })
+
+              .then(data => {
+                data.forEach(function(value){
+                  datass.push(value)
+                })
+                drawChart(datass)
+              });
+          } // for문 끝 요청부분 완료       
+        }
+      } // callback 함수 완료
       request(options, callback);
-      console.log(howmany)
-    }
-  }
+    }, // getRepose 끝
+
+    // ★★★★★★★★★  프로젝트 commit 가져오는 함수
+    getCommits(id) {
+      var request = require("request");
+      var headers = { "PRIVATE-TOKEN": "5yRamVkqs4Z4bq-G1roY" };
+      var options = {
+        url: "https://lab.ssafy.com/api/v4/projects/6097/repository/commits?",
+        headers: headers
+      };
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var howmany = response.headers["x-total"]; // 전체 아이템 수 계산 (27)
+          var total_try = parseInt(howmany / 100) + 1; // 한 페이지에 100개니까 얼마나 요청 보내야 하는지 계산 (27/100 + 1)
+
+          // total_try 만큼 요청보내기
+          for (var trying = 0; trying < total_try; trying++) {
+            fetch(
+              `${BASE_URL}/projects/6097/repository/commits??namespaces&per_page=100&page=${trying+1}&private_token=5yRamVkqs4Z4bq-G1roY`
+            )
+              .then(res => {
+                return res.json();
+              })
+              .then(data => {
+                this.ret = data;
+                console.log(data);
+              });
+          } // 요청부분 완료
+        }
+      } // callback 함수 정의 완료
+      request(options, callback);
+    } // getcommits 함수 완료
+  } // methods 정의 완료
 };
 </script>
 
 <style>
-.calendarContainer {
 
+
+@font-face { font-family: 'BBTreeGR'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_nine_@1.1/BBTreeGR.woff') format('woff'); font-weight: normal; font-style: normal; }
+.calendarContainer {
   position: relative;
-  width: 1000px;
-  height: 350px;
+  width: 100px;
+  height: 250px;
 }
 
 .calendarBox {
-
   position: absolute;
   top: 10%;
   left: 1%;
