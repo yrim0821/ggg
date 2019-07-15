c0e89eba3c1a609112fbdfaaa04d368b1d8d8b4c
김유림 <myccpb08@naver.com>
2019-07-15 14:10:10 +0900

    월요일

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -6,38 +6,29 @@
           <h2 v-line-clamp="1" class="font-weight-regular">{{repos.path_with_namespace}}</h2>
         </a>
 
-        <!-- dialog 테스트 --> 
+        <!-- dialog 테스트 -->
            <v-dialog v-model="dialog" width="500">
             <template v-slot:activator="{ on }">
-                <v-btn color="green" flat v-on="on" v-on:click="test(repos.owner.username)" >{{repos.owner.name}}</v-btn>
+                <v-btn color="green" flat v-on="on" v-on:click="test(repos.owner.username)" >{{repos.owner.name}}님 자세히</v-btn>
              </template>
 
       <v-card>
         <v-card-title  class="headline grey lighten-2" primary-title>
-          {{repos.owner.name}}님의 연간 활동
+          <a v-bind:href="'https://lab.ssafy.com/'+repos.owner.username">{{repos.owner.name}}</a>님의 연간 활동
+
         </v-card-title>
 
         <v-card-text>
+
            <!-- 미니그래프 테스트 -->
-                <v-container fluid>
-                  <v-btn >보기</v-btn>
-    <v-sparkline
-      :value="value"
-      :gradient="['#f72047', '#ffd200', '#1feaea']"
-      :smooth="radius || false"
-      :padding="padding"
-      :line-width="width"
-      :stroke-linecap="lineCap"
-      :gradient-direction="gradientDirection"
-      auto-draw
-    ></v-sparkline>
-
-  </v-container>
-
+            <div :id="repos.owner.username" style="height: 250px; width: 90%;"></div>
            <!-- 미니그래프 테스트 끝 -->
+           <v-divider></v-divider>
+             <!-- <h4 class="mt-3">{{ repos.owner.name }}님 프로젝트 <v-icon> arrow_downward</v-icon> </h4> -->
+
         </v-card-text>
 
-        <v-divider></v-divider>
+
 
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -58,16 +49,19 @@
   </div>
 </template>
 
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 <script>
+
 import GitlabService from "@/services/GitlabService";
 const BASE_URL = "https://lab.ssafy.com/api/v4";
 var tokens = {'myccpb08':'5yRamVkqs4Z4bq-G1roY', 'Kim_yh':'N9RKhWdxvbGzn3oYEwVe',
                       'JIGyeongmin':'yYcb5LEDsxxbN1PPxKEj', 'LeeSuKyeong':'dCp7MpuwFQNzYrLBZix5', 'seok':'xTftb51x12NTwFbxxAC5'}
 
 
-function calendar(datas){
+function calendar(datas,id){
   var graph = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0,'10':0,'11':0,'12':0}
   var act = []
+  var dataPoints = []
   if (datas != undefined) {
     // datas 로 반복문 돌림
     for (let index = 0; datas[index] != null; index++) {
@@ -78,9 +72,32 @@ function calendar(datas){
 }
   for(var key in graph){
     act.push(graph[key])
+    dataPoints.push({x:parseInt(key), y:graph[key]})
   }
-   
+  mini(dataPoints,id)
+
+
 }
+}
+
+
+function mini(data,id) {
+var chart = new CanvasJS.Chart(id, {
+  animationEnabled: true,
+  data: [{
+    yValueFormatString: "#,###",
+    xValueFormatString: "#,###",
+    type: "spline",
+    lineColor: "blue",
+    dataPoints: data,
+    lineThickness: 5,
+     markerType:'circle',
+     markerColor:'red'
+  }]
+
+});
+chart.render();
+
 }
 
 
@@ -93,13 +110,6 @@ export default {
     return {
       dialog : false,
       stats: {},
-      value: [1,1,3,1,],
-      width: 3.5,
-      radius: 5,
-      padding: 4,
-      lineCap: 'round',
-      gradient: ['#f72047', '#ffd200', '#1feaea'],
-      gradientDirection: 'top',
     };
   },
   mounted() {
@@ -124,7 +134,7 @@ export default {
 
           // total_try 만큼 요청보내기
           for (var trying = 0; trying < total_try; trying++) {
-            
+
             fetch(
               `${BASE_URL}/users/${id}/events?namespaces&per_page=100&page=${trying +
                 1}&private_token=${token}`
@@ -136,9 +146,9 @@ export default {
                 data.forEach(function(value) {
                   datass.push(value);
                 });
-                calendar(datass)
+                calendar(datass,id)
           })
-          
+
           } // for문 끝 요청부분 완료
 
       }} // callback 함수 완료
@@ -148,30 +158,6 @@ export default {
     test(username) {
       this.getRepos(username, tokens[username]);
     },
-
-
-  calendar(datas){
-  var graph = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0,'10':0,'11':0,'12':0}
-  var act = []
-  if (datas != undefined) {
-    // datas 로 반복문 돌림
-    for (let index = 0; datas[index] != null; index++) {
-      var data = datas[index].created_at;
-      if (data == null) continue;
-      var month = new Date(data).getMonth() + 1;
-        graph[month]++
-}
-  for(var key in graph){
-    act.push(graph[key])
-  }
-  this.value = act
-   
-}
-}
-
-
-
-
     } //method
 }; // default
 </script>
